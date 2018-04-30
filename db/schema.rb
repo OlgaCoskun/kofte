dff101b0b5984e4e6f21d49132eb097895ef2c11
Olga Coskun <ovin999@gmail.com>
2018-04-30 13:30:22 +0300

    Fix errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426123140) do
+ActiveRecord::Schema.define(version: 20180430061443) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "user_name"
+    t.integer "event_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
