f553b6936d7b0df8b16c219a2373e69ab4286f27
Olga Coskun <ovin999@gmail.com>
2018-05-02 10:50:20 +0300

    Add users name after sign_in

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -7,18 +7,16 @@ class SubscriptionsController < ApplicationController
 
 
   def create
-    if current_user == @event.user_id
-      # Болванка для новой подписки
-      @new_subscription = @event.subscriptions.build(subscription_params)
-      @new_subscription.user = current_user
+    # Болванка для новой подписки
+    @new_subscription = @event.subscriptions.build(subscription_params)
+    @new_subscription.user = current_user
 
-      if @new_subscription.save
-        # Если сохранилась успешно, редирект на страницу самого события
-        redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
-      else
-        # если ошибки — рендерим здесь же шаблон события
-        render 'events/show', alert: I18n.t('controllers.subscriptions.error')
-      end
+    if @new_subscription.save
+      # Если сохранилась успешно, редирект на страницу самого события
+      redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
+    else
+      # если ошибки — рендерим здесь же шаблон события
+      render 'events/show', alert: I18n.t('controllers.subscriptions.error')
     end
   end
 
@@ -35,6 +33,7 @@ class SubscriptionsController < ApplicationController
   end
 
   private
+
   def set_subscription
     @subscription = @event.subscriptions.find(params[:id])
   end
