b5a36ee715e849bf63252f537cc8dbbb9a84f18b
Olga Coskun <ovin999@gmail.com>
2018-05-02 22:55:00 +0300

    Add css for fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
+  # дергаем спец. девайзовский метод, который генерит все нужные ему пути
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "events#index"
 
   resources :events do
