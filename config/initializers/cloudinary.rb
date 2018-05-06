7b5170bf758f5d304d409931b69e0cc151ca8c78
Olga Coskun <ovin999@gmail.com>
2018-05-06 18:53:29 +0300

    Fix gitignore

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = "cloudolga"
-  config.api_key = "144655146287369"
-  config.api_secret = "f9vbfzfxtnE1K1gkO0YKL8Janjw"
-  config.cdn_subdomain = true
-  config.enhance_image_tag = true
-end
