a5e0af11934628dfa31cb91cb1138e43be522dc5
Olga Coskun <ovin999@gmail.com>
2018-05-07 11:13:33 +0300

    settings

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,7 +2,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
