cf731e04169a87515ed6e3325f3f5386b234effc
Olga Coskun <ovin999@gmail.com>
2018-05-14 17:04:08 +0300

    Fix mailer photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -66,7 +66,7 @@ class PhotosController < ApplicationController
   def notify_subscribers(event, photo)
     # собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-    #all_emails.delete_if {|email| email == current_user.email}
+    all_emails.delete_if {|email| email == current_user.email}
 
     # XXX: Этот метод может выполняться долго из-за большого числа подписчиков
     # поэтому в реальных приложениях такие вещи надо выносить в background задачи!
