ca69677834639c2aa063bd60989f5ed1f97230b2
Olga Coskun <ovin999@gmail.com>
2018-05-17 15:30:58 +0300

    back config

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  # дергаем спец. девайзовский метод, который генерит все нужные ему пути
+  devise_for :users
+
+  root "events#index"
+
+  resources :events do
+    # вложенный ресурс комментов
+    resources :comments, only: [:create, :destroy]
+
+    # вложенный ресурс подписок (см. rake routes)
+    resources :subscriptions, only: [:create, :destroy]
+
+    # Вложенные в ресурс события ресурсы фотографий
+    resources :photos, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+end
